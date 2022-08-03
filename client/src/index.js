a453154396439992393aba1c8f91eba68b31cde1
trv893 <trevorhilimire@gmail.com>
2022-08-03 13:53:11 -0400

    connects bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter as Router} from 'react-router-dom'
-import App from './App'; 
+import App from './App';
+// imports bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
