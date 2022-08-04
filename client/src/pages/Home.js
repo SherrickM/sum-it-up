ff01cf4b11b54146535313a9495e8a9f35c3ee30
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-08-03 20:56:25 -0400

    column layout for home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,12 +4,24 @@ export default function Home(){
   return (
     <div className="container">
         <h1 className="headline">Home</h1>
-        <div className="row">
-		    <div className="col-md-6">
-                Left col - This section will be an summary of the APP
+        <div className="d-flex gap-3 mb-4">
+		    <div className="col-md-6 border border-danger rounded p-4">
+                <h2>This section will be an summary of the APP</h2>
+                <ul>
+                    <li>Intro section</li>
+                    <li>About the project</li>
+                    <li>Images/Carousel</li>
+                    <li>signup Call to action</li>
+                </ul>
             </div>
-		    <div className="col-md-6">
-                right col - This section will host a trial version (1 free summary before signing up)
+		    <div className="col-md-6 border border-danger rounded p-4">
+                <h2>This section will host a trial version (1 free summary before signing up)</h2>
+                <ul>
+                    <li>Input field</li>
+                    <li>button to summarize</li>
+                    <li>replace input field with summarized article</li>
+                </ul>
+
             </div>
 	    </div>                    
     </div>
