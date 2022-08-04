93158f78f8600131e5a0d11238594028695786b7
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-08-03 20:44:46 -0400

    home layout update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
+import React from "react";
 
-const Home = () => {
-    return (
-        <div>
-            <Button variant="primary">Primary</Button>{' '}
-        </div>
-    )
+export default function Home(){
+  return (
+    <div className="container">
+        <h1 className="headline">Home</h1>
+        <div className="row">
+		    <div className="col-md-6">
+                Left col - This section will be an summary of the APP
+            </div>
+		    <div className="col-md-6">
+                right col - This section will host a trial version (1 free summary before signing up)
+            </div>
+	    </div>                    
+    </div>
+  );
 }
-
-export default Home
