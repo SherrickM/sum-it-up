bfc56d3300d91f36d3f0d439a37339e9c8b90c88
Marquis <smcneal89@gmail.com>
2022-08-10 12:56:24 -0400

    removed dotenv info

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,9 +4,13 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    user: async (_parent, { email }) => {
-      return User.findOne({ email })
-        .select('-__v -password')
+    me:  async (parent, args, context) => {
+      if(context.user){
+        const userData = await User.findOne({_id: context.user._id}).select('-__v -password');
+
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in!')
     },
   },
   Mutation: {
