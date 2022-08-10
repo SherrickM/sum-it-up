0201fd5972e14cbbe906701758a79c5a0b091ac1
Marquis <smcneal89@gmail.com>
2022-08-10 18:24:32 -0400

    updated connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(
 
-  process.env.MONGODB_ENDPOINT || 'mongodb://127.0.0.1:27017/sumitupapp',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sumitupapp',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
