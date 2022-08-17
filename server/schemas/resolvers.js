f76145d42da64936abcdcd75f0a93ad37e67708d
trv893 <trevorhilimire@gmail.com>
2022-08-17 10:39:52 -0400

    fixes minor bug in syntax in getSummary query in resolvers and obscures key

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,37 +1,51 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const fetch = require("node-fetch")
+require('dotenv').config()
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const fetch = require("node-fetch");
 
-const { signToken } = require('../utils/auth');
+console.log(process.env)
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    me:  async (parent, args, context) => {
-      if(context.user){
-        const userData = await User.findOne({_id: context.user._id}).select('-__v -password');
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
 
         return userData;
       }
-      throw new AuthenticationError('Not logged in!')
+      throw new AuthenticationError("Not logged in!");
     },
-    getSummary:(text, sentnum) => {
-      const url = 'https://textanalysis-text-summarization.p.rapidapi.com/text-summarizer';
+    getSummary: (_, data) => {
+      let {text, sentnum} = data;
+      const encodedParams = new URLSearchParams();
+      encodedParams.append(
+        "text",
+        text
+      );
+      encodedParams.append("sentnum", sentnum);
+
+      const url =
+        "https://textanalysis-text-summarization.p.rapidapi.com/text-summarizer-text";
 
       const options = {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'content-type': 'application/json',
-          'X-RapidAPI-Key': '182714b1e6mshb412212f9fdf6e5p13fb70jsn4cac94f5b2c6',
-          'X-RapidAPI-Host': 'textanalysis-text-summarization.p.rapidapi.com'
+          "content-type": "application/x-www-form-urlencoded",
+          "X-RapidAPI-Key":
+          process.env.apikey,
+          "X-RapidAPI-Host": "textanalysis-text-summarization.p.rapidapi.com",
         },
-        body: `{"text":`+{text}+`,"sentnum":`+{sentnum}+`}`
-        };
+        body: encodedParams,
+      };
 
-        fetch(url, options)
-          .then(res => res.json())
-          .then(json => console.log(json))
-          .catch(err => console.error('error:' + err));
-    }
+      fetch(url, options)
+        .then((res) => res.json())
+        .then((json) => console.log(json))
+        .catch((err) => console.error("error:" + err));
+    },
   },
   Mutation: {
     addUser: async (_parent, args) => {
@@ -44,20 +58,19 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('Incorrect Username');
+        throw new AuthenticationError("Incorrect Username");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect Password');
+        throw new AuthenticationError("Incorrect Password");
       }
 
       const token = signToken(user);
       return { token, user };
-    }
+    },
   },
-  
 };
 
 module.exports = resolvers;
