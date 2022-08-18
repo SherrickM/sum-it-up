53735be9fdfa61546dd81b476d3a90b0720e1e27
trv893 <trevorhilimire@gmail.com>
2022-08-17 21:45:32 -0400

    package file conflicxts

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,20 @@
-import gql from 'graphql-tag';
+// import gql from 'graphql-tag';
+import { useQuery } from "react-query";
+import { GraphQLClient, gql } from "graphql-request";
+
+export function useGetSummary(data) {
+    return useQuery(["getSummary", data], async () => {
+      const { getSummary } = await graphQLClient.request(
+        gql`
+          query getSummary(text: String!, sentnum: Int) {
+            getPost(text: $data: text) {
+              test
+              sentnum
+            }
+          }
+        `,
+        { postId }
+      );
+      return getPost;
+    });
+  }z
