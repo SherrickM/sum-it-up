6d47818eb29e8f7445bc69eccfa3e9ecc824871f
trv893 <trevorhilimire@gmail.com>
2022-08-19 12:19:48 -0400

    switch from rapidapi to azure in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,58 @@
-require('dotenv').config()
+var z = require('dotenv').config()
 const { AuthenticationError } = require("apollo-server-express");
 const { User, Folder } = require("../models");
 const fetch = require("node-fetch");
 const { signToken } = require("../utils/auth");
+const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
+
+
+var getClient = function()
+{
+  const key = z.parsed.apikey;
+  const endpoint = z.parsed.endpoint;
+  // Authenticate the client with your key and endpoint
+  const textAnalyticsClient = new TextAnalyticsClient(endpoint, new AzureKeyCredential(key));
+  return textAnalyticsClient;
+}
+
+async function getAzSummary(text, sentnum)
+{
+  var client = getClient();
+  const documents = [text]
+  // const documents = [`The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document. 
+  // These sentences collectively convey the main idea of the document. This feature is provided as an API for developers. 
+  // They can use it to build intelligent solutions based on the relevant information extracted to support various use cases. 
+  // In the public preview, extractive summarization supports several languages. It is based on pretrained multilingual transformer models, part of our quest for holistic representations. 
+  // It draws its strength from transfer learning across monolingual and harness the shared nature of languages to produce models of improved quality and efficiency.`];
+
+
+  const actions = {
+    extractSummaryActions: [{ modelVersion: "latest", orderBy: "Rank", maxSentenceCount: sentnum }],
+  };
+  const poller = await client.beginAnalyzeActions(documents, actions, "en");
+
+  const resultPages = await poller.pollUntilDone();
+  var r = [];
+
+  for await (const page of resultPages) {
+    const extractSummaryAction = page.extractSummaryResults[0];
+    if (!extractSummaryAction.error) {
+        for (const doc of extractSummaryAction.results) {
+            console.log(`- Document ${doc.id}`);
+            if (!doc.error) {
+                console.log("\tSummary:");
+                for (const sentence of doc.sentences) {
+                    r.push(sentence.text);
+                }
+            } else {
+                console.error("\tError:", doc.error);
+            }
+        }
+    }
+  } 
+
+  return r.join(" ");
+}
 
 const resolvers = {
   Query: {
@@ -18,35 +68,8 @@ const resolvers = {
     },
     getSummary: async (_, data) => {
       let {text, sentnum} = data;
-      const encodedParams = new URLSearchParams();
-      encodedParams.append(
-        "text",
-        text
-      );
-      encodedParams.append("sentnum", sentnum);
-
-      const url =
-        "https://textanalysis-text-summarization.p.rapidapi.com/text-summarizer-text";
-
-      const options = {
-        method: "POST",
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-          "X-RapidAPI-Key":
-          "47a5a67642msh009c5b0acb21432p1fd494jsn96c3b9ceacca",
-          "X-RapidAPI-Host": "textanalysis-text-summarization.p.rapidapi.com",
-        },
-        body: encodedParams,
-      };
-
-      var sumval = await fetch(url, options)
-        .then((res) => res.json())
-        .then((json) => {
-          return json})
-        .catch((err) => console.error("error:" + err));
-        console.log(typeof(sumval));
-        var rs=  sumval.sentences.join(' ');
-        return {sentences:rs};
+      var r = await getAzSummary(text, sentnum);
+      return {sentences:r};
     },
   },
   Mutation: {
