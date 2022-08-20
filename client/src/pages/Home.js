817d30b3a9fe30273eceb0bb4bf86598e9f41819
Gedion Adamu <gedionadamu@gmail.com>
2022-08-20 13:21:01 -0400

    added ability to summarize in homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Profile from "./Profile";
+import TextSummarySubmit from "../components/SummarySubmitandSave";
 
 export default function Home(){
   return (
     <>
-    
+    <TextSummarySubmit/>
     </>
  
     
