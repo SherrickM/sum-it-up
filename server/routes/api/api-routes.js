fde75cb6719faea613e7083e7fd8b1d3427baed0
trv893 <trevorhilimire@gmail.com>
2022-08-22 19:03:40 -0400

    pull from main merge

--- server/routes/api/api-routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const  { getSummary } = require("../../controllers/summary-controller");
-
-// router.route('/').post(getSummary);
-
-
-module.exports = router;
