13f5179a83fc6ae831bf3e69614235be5b37666b
Christiana <christianatroyon@gmail.com>
2022-08-22 14:51:58 -0400

    added code to save summaries to the user

--- client/src/components/SummarySubmitandSave.js
+++ client/src/components/SummarySubmitandSave.js
@@ -1,7 +1,8 @@
 import React, { useState, useReducer } from "react";
 import summation from './summationsymble.png'
-import { useQuery, useMutation } from '@apollo/client';
-import { ADD_SUMMARY } from "../utils/mutations";
+import { useMutation } from '@apollo/client';
+import { ADD_SUMMARY, LOGIN_USER } from "../utils/mutations";
+import { QUERY_SUMMARIES, QUERY_ME } from "../utils/queries";
 import Auth from '../utils/auth';
 
 
@@ -24,152 +25,164 @@ const TextSummarySubmit = () => {
   const [summarizedState, setsummarizedState] = useState("");
   const [sentenceNum, setsentenceNum] = useState(1)
 
-  // const [addSummary, { data, loading, error }] = useMutation(ADD_SUMMARY);
-
-  
 
   const handleChange = (event) => {
     const value = event.target.value;
     setFormState(value);
-    
+
   };
   const handleInputChange = (event) => {
     const value = event.target.value;
     setsentenceNum(value);
   };
-  const handleSaveSummary =async(event) => {
 
-    // event.preventDefault();
-
-    // try {
-    //   const { data } = await addSummary({
-    //     variables: {
-    //       user:Auth.getProfile().data.username,
-    //       summarizedState,
-          
-    //     },
-    //   });
-
-    // } catch (err) {
-    //   console.error(err);
-    // }
-
-    // const summary = summarizedState;
-    // console.log(summary);
-    // ADD_SUMMARY({ variables: { summaryText: summary } });
-
-  };
-
-
-  const onFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
-    try {
-      var options = {
-        method: 'POST',
-        url: process.env.REACT_APP_APIURL + '/api/sumapi',
-        headers: { 'Content-Type': 'application/json' },
-        data: {
-          text: formState,
-          sentnum: parseInt(sentenceNum),
-        }
-      };
-
-      var r = await axios.request(options).then(function (response) {
-        console.log(response.data.sentences);
-        setsummarizedState(response.data.sentences);
-        console.log(summarizedState);
-
-
+  const [addSummary, { error }] = useMutation(ADD_SUMMARY, {
+    update(cache, { data: { addSummary } }) {
+      try {
+        const { summaries } = cache.readQuery({ query: QUERY_SUMMARIES });
+      } catch (e) {
+        console.error(e);
+      }
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, summaries: [...me.summaries, addSummary] } },
       });
     }
-    catch (e) {
-      console.error(e);
+  });
 
+  const handleSaveSummary = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addSummary({
+        variables: {
+          user: Auth.getProfile().data.username,
+          summarizedState,
+
+        },
+      });
+
+    } catch (err) {
+      console.error(err);
     }
-  };
 
-  // const result = await login({
-  //   variables: { ...formState },
-  // });
-  // console.log(result);
-  // Auth.login(dispatch, result.data.login.token, result.data, navigate);
-  return (
-    
+    const summary = summarizedState;
+    console.log(summary);
+    ADD_SUMMARY({ variables: { summaryText: summary } });
+
+  }
 
 
-    <section className="content-section">
-      <div className="section-header-wrapper">
-        <h1 className="section-header">Add text to Summarize</h1>
-        <h1 className="section-header">Summary</h1>
-        
-      </div>
-      <div className="content-section-line">
-        <div className="section-part left">
-          <div className="image-wrapper">
-            <div className="image-overlay">
-              <div className="video-info">
-                <div className="video-info-text">
-                  <form className="section-part left" onSubmit={onFormSubmit}>
-                    Add Discription Of The Summary
-                    <div className="search-wrapper mt-3 file-name" id="summary-input-area">
-                      <textarea rows="20" cols="50" className="input" type="text" name="text" value={formState.text} onChange={handleChange} placeholder="Paste Text to Summarize here" />
-                    </div>
+const onFormSubmit = async (event) => {
+  event.preventDefault();
+  console.log(formState);
+  try {
+    var options = {
+      method: 'POST',
+      url: process.env.REACT_APP_APIURL + '/api/sumapi',
+      headers: { 'Content-Type': 'application/json' },
+      data: {
+        text: formState,
+        sentnum: parseInt(sentenceNum),
+      }
+    };
 
-                    <div class="search-wrapper mt-1 file-catagory" id="summary-sentance-num">
-                      <input class="input" type="number" name="sentenceNum" value={sentenceNum.sentenceNum} onChange={handleInputChange} placeholder="Number of sentences e.g. 3 or 5. Default is 1" />
-                    </div>
-
-                    <button  type="submit" class="btn btn-secondary btn-main m-2 submit_for_summery">Summarize!</button>
+    var r = await axios.request(options).then(function (response) {
+      console.log(response.data.sentences);
+      setsummarizedState(response.data.sentences);
+      console.log(summarizedState);
 
 
-                  </form>
-                </div>
+    });
+  }
+  catch (e) {
+    console.error(e);
 
+  }
+};
+
+// const result = await login({
+//   variables: { ...formState },
+// });
+// console.log(result);
+// Auth.login(dispatch, result.data.login.token, result.data, navigate);
+return (
+
+
+
+  <section className="content-section">
+    <div className="section-header-wrapper">
+      <h1 className="section-header">Add text to Summarize</h1>
+      <h1 className="section-header">Summary</h1>
+
+    </div>
+    <div className="content-section-line">
+      <div className="section-part left">
+        <div className="image-wrapper">
+          <div className="image-overlay">
+            <div className="video-info">
+              <div className="video-info-text">
+                <form className="section-part left" onSubmit={onFormSubmit}>
+                  Add Discription Of The Summary
+                  <div className="search-wrapper mt-3 file-name" id="summary-input-area">
+                    <textarea rows="20" cols="50" className="input" type="text" name="text" value={formState.text} onChange={handleChange} placeholder="Paste Text to Summarize here" />
+                  </div>
+
+                  <div class="search-wrapper mt-1 file-catagory" id="summary-sentance-num">
+                    <input class="input" type="number" name="sentenceNum" value={sentenceNum.sentenceNum} onChange={handleInputChange} placeholder="Number of sentences e.g. 3 or 5. Default is 1" />
+                  </div>
+
+                  <button type="submit" class="btn btn-secondary btn-main m-2 submit_for_summery">Summarize!</button>
+
+
+                </form>
               </div>
+
             </div>
-            <img src="https://images.unsplash.com/photo-1492691527719-9d1e07e534b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80" />
-
           </div>
+          <img src="https://images.unsplash.com/photo-1492691527719-9d1e07e534b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80" />
 
         </div>
 
-        <div className="section-header-wrapper">
+      </div>
+
+      <div className="section-header-wrapper">
 
 
-        </div>
+      </div>
 
-        <div className="files-table w-50 ">
+      <div className="files-table w-50 ">
 
-          <div className="files-table-header">
+        <div className="files-table-header">
 
-            <ol>
-              <li className="files-table-row"> Summary one</li>
-              <li className="files-table-row"> Summary two</li>
-              <li className="files-table-row"> Summary Three</li>
-            </ol>
-            <form>
-              <div className="search-wrapper mt-3 file-name mb-5" >
-                <input className="input" type="text" name="project" value={formState.project} onChange={handleChange} placeholder="Name of Project e.g. Climate Change" />
-              </div>
-              <div className="search-wrapper mt-1 file-catagory mb-5">
-                <input className="input" type="text" placeholder="Catagory e.g. Science" />
-              </div>
-              <button onClick={handleSaveSummary} type="save" class="btn btn-secondary btn-main submit_for_summery">Save summary!</button>
-          
+          <ol>
+            <li className="files-table-row"> Summary one</li>
+            <li className="files-table-row"> Summary two</li>
+            <li className="files-table-row"> Summary Three</li>
+          </ol>
+          <form>
+            <div className="search-wrapper mt-3 file-name mb-5" >
+              <input className="input" type="text" name="project" value={formState.project} onChange={handleChange} placeholder="Name of Project e.g. Climate Change" />
+            </div>
+            <div className="search-wrapper mt-1 file-catagory mb-5">
+              <input className="input" type="text" placeholder="Catagory e.g. Science" />
+            </div>
+            <button onClick={handleSaveSummary} type="save" class="btn btn-secondary btn-main submit_for_summery">Save summary!</button>
+
           </form>
-          </div>
+        </div>
 
-          
-          
+
+
       </div>
     </div>
 
-    </section >
+  </section >
 
 
 
-  );
-};
+)};
+
 
 export default TextSummarySubmit;
