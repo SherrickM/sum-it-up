d45d491dbbe5fb1c5718e92c21adeb9e68d7d4ff
Gedion Adamu <gedionadamu@gmail.com>
2022-08-22 20:33:17 -0400

    cleaned up unused code

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,8 @@
 
-import {
-  AppBar, Toolbar, List, Typography
-} from '@material-ui/core';
-import Box from "@material-ui/core/Box";
+
+
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+
 import Auth from '../../utils/auth';
 import Logo from '../../assets/images/sum-it-up-logo.png'
 
